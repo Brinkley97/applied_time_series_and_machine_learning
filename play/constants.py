0cf708f5726e7cf00491ba1a0bc8e068078ecf79
Detravious J. Brinkley <d.brinkley97@gmail.com>
2023-09-15 11:51:17 -0700

    updated constants.py

--- play/constants.py
+++ play/constants.py
@@ -1,8 +1,9 @@
-from typing import List, Tuple, NewType, Union, Any, TypeAlias
+from typing import List, Tuple, NewType, Union, Any #, TypeAlias
 
 Number = Union[int, float]
 Numbers = List[Number]
-TimeSeriesObservation: TypeAlias = Number
+# TimeSeriesObservation: TypeAlias = Number
+TimeSeriesObservation = Union[int, float]
 TimeSeriesRealization = NewType("TimeSeriesRealization", List[TimeSeriesObservation])
 UnivariateTimeSeriesData = NewType("UnivariateTimeSeriesData", TimeSeriesRealization)
 MultivariateTimeSeriesData = NewType("MultivariateTimeSeriesData", List[UnivariateTimeSeriesData])
