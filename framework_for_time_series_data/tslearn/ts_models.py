fbe3da331dbde6d0bf46ed5dfecee54190345b88
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-02-15 13:30:06 -0800

    implementing MLP

--- framework_for_time_series_data/tslearn/ts_models.py
+++ framework_for_time_series_data/tslearn/ts_models.py
@@ -1,9 +1,10 @@
 import matplotx
+import torch
 
 import numpy as np
 import pandas as pd
 
-# import torch.nn as nn
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
 from abc import ABC
@@ -33,6 +34,8 @@ class Model(ABC):
     def augment_data(self):
         pass
 
+    def forward(self):
+        pass
 
     def predict(self):
         pass
@@ -345,9 +348,16 @@ class ARIMA_model(Model):
 
         return predictions
 
-class MLP(Model):
+class MLP(nn.Module):
     def __name__(self):
         return "MLP"
+
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
 
     def augment_data(uts_observations, prior_observations: int, forecasting_step: int) -> pd.DataFrame:
         """Splits a given UTS into multiple input rows where each input row has a specified number of timestamps and the output is a single timestamp.
@@ -394,6 +404,11 @@ class MLP(Model):
         # last column
         y_train = uts_sml_df.iloc[:, [-1]]
         return uts_sml_df, X_train, y_train
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 @dataclass
 class EvaluationMetric:
