5a03c07d2c5d0589a61b83ba5ce558212e5fce8e
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-04-29 13:58:55 -0700

    Keras and PyTorch for MLP

--- framework_for_time_series_data/tslearn/ml_models.py
+++ framework_for_time_series_data/tslearn/ml_models.py
@@ -27,14 +27,15 @@ class MLP(nn.Module):
     def __name__(self):
         return "MLP"
 
-
     def __init__(self, input_size, hidden_size, output_size):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        
 
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        fc1_out = self.fc1(x)
+        relu_out = self.relu(fc1_out)
+        fc2_out = self.fc2(relu_out)
+        return fc2_out
