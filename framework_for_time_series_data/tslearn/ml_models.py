0a869d067ffd8c1d2b23d1201414c53939b086a6
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-04-30 12:45:15 -0700

    updating with train and predict functions

--- framework_for_time_series_data/tslearn/ml_models.py
+++ framework_for_time_series_data/tslearn/ml_models.py
@@ -27,7 +27,7 @@ class MLP(nn.Module):
     def __name__(self):
         return "MLP"
 
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
@@ -39,3 +39,42 @@ class MLP(nn.Module):
         relu_out = self.relu(fc1_out)
         fc2_out = self.fc2(relu_out)
         return fc2_out
+    
+    def train(self, input_size: int, hidden_size: int, output_size: int, config: list):
+        """Train the MLP for #epoch
+
+        """
+        criterion, optimizer = config
+
+        X = torch.randn(hidden_size, input_size)  # Example input data
+        y = torch.randn(hidden_size, output_size)  # Example target data
+
+        epochs = 2000
+        for epoch in range(epochs):
+            # Forward pass
+            outputs = self(X)
+            loss = criterion(outputs, y)
+            
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if (epoch+1) % 100 == 0:
+                print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}')
+    
+    def predict(self, data_df: pd.DataFrame, input_size: int):
+        """
+        """
+
+        x_input_df = data_df.iloc[[-1], -input_size:].copy()
+        x_input_tensor = torch.tensor(x_input_df.values, dtype=torch.float32)
+        print("x_input_tensor shape:", x_input_tensor.shape, x_input_tensor)
+
+        yhat = self(x_input_tensor)  # Perform forward pass
+        
+        # Extract predicted values for each sample in the batch
+        predicted_values = yhat.squeeze(dim=1).tolist()  # Convert tensor to list of predicted values
+        print("Predicted Outputs:", predicted_values)
+        
+        return predicted_values
