4a5c58fa977408c5e4dbcd78efe350b4efa9fc47
Detravious Jamari Brinkley <dj.brinkley@ufl.edu>
2025-07-05 00:13:43 -0400

    feat: render blog posts on page with user and more

--- tslearn_app/blog/views.py
+++ tslearn_app/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
     """templates/blog/home.html"""
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     """templates/blog/about.html"""
